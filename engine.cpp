2744a83be8e70d9fd88e5b1facb973e035e8f988
muhtasham-data <muhtasham.data.ai@gmail.com>
2026-09-17 06:45:20 +0500

    completed

---
+++ engine.cpp
@@ -0,0 +1,51 @@
+#include"engine.hpp"
+#include<iostream>
+using std::cout,std::cin,std::getline;engine master;
+void engine::getquery(){
+    cout<<"Enter your Query: ";
+    getline(cin,query);
+    o.open("data/query");
+    o<<query;
+    o.close();
+}
+string engine::cleanWord(const string& input) {
+    string result = "";
+    for (char ch : input) {
+        if (!ispunct(static_cast<unsigned char>(ch))) {
+            result += tolower(static_cast<unsigned char>(ch));
+        }
+    }
+    return result;
+}
+void engine::search(string filename){
+    int count=0;
+    string filepath="data/"+filename;
+    i.clear();
+    i.open("data/query");
+    while(i>>dataq){
+        string data1=cleanWord(dataq);
+        ifstream n(filepath);
+        while (n>>dataa){
+            string data2=cleanWord(dataa);
+            if(data1==data2){
+                count++;
+            }
+        }
+        n.close();
+    }
+    i.close();
+    db.emplace_back(filename,count);
+}
+void engine::output(){
+    std::sort(db.begin(),db.end(),[](const auto &a,const auto &b){return a.count>b.count;});
+    for(const auto &x:db){
+        cout<<"In "<<x.query<<": "<<x.count<<"\n";
+    }
+}   
+int main(){
+    master.getquery();
+    master.search("doc1.txt");
+    master.search("doc2.txt");
+    master.search("doc3.txt");
+    master.output();
+}
--- engine.hpp
+++ engine.hpp
@@ -1,13 +1,22 @@
 #pragma once
 #include<vector>
 #include<fstream>
+#include<algorithm>
+#include<sstream>
+#include<cctype>
 using std::string,std::vector,std::ofstream,std::ifstream;
 struct engine{
     struct data{
         int count;
-        string query,filename;
+        string query;
+        data(string a,int b):query(a),count(b){}
     };
+    int count;string query,filename,dataa,dataq;
     ofstream o;ifstream i;
     vector<data>db;
+    void getquery();
     void search(string filename);
+    void output();
+    string cleanWord(const string& input);
 };
+extern engine master;
